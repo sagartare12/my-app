d1a22c5a1dd935212619de77d7b9cd203b3db062
sagar <sagartare2014@gmail.com>
2021-03-02 21:08:29 +0530

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey clever programmer , lets build clone</h1>
+
+     
+    </div>
+  );
+}
+
+export default App;
