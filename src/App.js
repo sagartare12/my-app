ed7958c0d3d34323798e43580307aafc3dadfc7e
sagar <sagartare2014@gmail.com>
2021-03-06 14:28:23 +0530

    Add images below the navBar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 //import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
+import { Navbar, NavbarBrand} from 'reactstrap'
+import { render } from '@testing-library/react';
+import MenuComponent from './component/MenuComponent';
 
-
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
-      <h1>Hey clever programmer , lets build clone</h1>
-
+      <Navbar dark color="primary">
+        <NavbarBrand href="/">Sagar
+        </NavbarBrand>
+      </Navbar>
+      <MenuComponent />
+     
      
     </div>
   );
+  }
 }
 
 export default App;
